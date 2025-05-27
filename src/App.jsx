f68f65050a8cba741b43a33236ccac44ac9a5862
Strong-Moon <pehlivan.berkay2001@gmail.com>
2025-05-27 22:38:56 +0300

    compoenents are added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login.jsx";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
